7ee142325293a9e1425004b32d0a3167b4efa19e
Dan Freeman <dfreeman@salsify.com>
2016-02-16 12:52:04 -0500

    Actually properly test the initializer in 1.13

---
+++ tests/unit/initializers/ember-css-modules-test.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import Initializer from '../../../initializers/ember-css-modules';
+
+let application;
+
+module('Unit | Initializer | ember css modules', {
+  beforeEach() {
+    Ember.run(function() {
+      application = Ember.Application.create();
+      application.deferReadiness();
+    });
+  }
+});
+
+test('registers options for the `styles` container entity type', function(assert) {
+  // 1.13.x passes the registry rather than the application
+  let app = application.registry || application;
+  let registeredOptionsForType = app.registeredOptionsForType || app.getOptionsForType;
+
+  Initializer.initialize(app);
+
+  assert.deepEqual(registeredOptionsForType.call(app, 'styles'), {
+    instantiate: false
+  });
+});
