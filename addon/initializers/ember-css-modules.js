a31f0b07d821f5ac69fb6dd02c782e9ae16e91d9
Dan Freeman <dfreeman@salsify.com>
2016-03-31 13:39:46 -0700

    Remove the need for custom registry options.
    Closes #14.

--- addon/initializers/ember-css-modules.js
+++ addon/initializers/ember-css-modules.js
@@ -1,11 +1,7 @@
 // Ensure our core extensions are loaded before the app boots
 import 'ember-css-modules/extensions';
 
-export function initialize(application) {
-  // For 1.13.x compat
-  let registerOptions = application.registerOptionsForType || application.optionsForType;
-  registerOptions.call(application, 'styles', { instantiate: false });
-}
+export function initialize() {}
 
 export default {
   name: 'ember-css-modules',
