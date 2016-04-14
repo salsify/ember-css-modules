d1f7069f23fa3d28bade4b36b7f472c08ab898ff
Dan Freeman <dfreeman@salsify.com>
2016-04-14 17:47:23 -0400

    Update ember-try and clean up testing

--- addon/mixins/component-lookup-mixin.js
+++ addon/mixins/component-lookup-mixin.js
@@ -9,14 +9,16 @@ export default Ember.Mixin.create({
 
     // Ensure components are always managed my the container and thus have a connection to their styles
     if (!component) {
-      if (owner.register) {
-        owner.register(`component:${name}`, Ember.Component);
-      } else {
-        owner._registry.register(`component:${name}`, Ember.Component); // Support for Ember 2.0.X
-      }
+      findRegistry(owner).register(`component:${name}`, Ember.Component);
       component = this._super(name, owner, options);
     }
 
     return component;
   }
 });
+
+// There are like a dozen different ways of registering something across our various supported versions of Ember,
+// all varying levels of public-ish. This threads that needle without triggering deprecation warnings.
+function findRegistry(owner) {
+  return owner._registry || (owner.register ? owner : owner.registry);
+}
--- config/ember-try.js
+++ config/ember-try.js
@@ -1,42 +1,4 @@
 /*jshint node:true*/
 module.exports = {
-  scenarios: [
-    {
-      name: 'default',
-      dependencies: { }
-    },
-    {
-      name: 'ember-lts',
-      dependencies: {
-        'ember': '1.13'
-      }
-    },
-    {
-      name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
-        'ember': 'release'
-      }
-    },
-    {
-      name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
-      }
-    },
-    {
-      name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
-      }
-    }
-  ]
+  useVersionCompatibility: true
 };
