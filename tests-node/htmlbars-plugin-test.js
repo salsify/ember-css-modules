8da67a0bfc1f6174a026a0d721fffa4aa79a71d6
Kevin Yang <kyang@salsify.com>
2016-08-16 11:50:33 -0400

    fix double handlebars

--- tests-node/htmlbars-plugin-test.js
+++ tests-node/htmlbars-plugin-test.js
@@ -40,7 +40,7 @@ testTransformation('appending to an unquoted class attribute with multiple class
 
 testTransformationElement('creating a class attribute with dynamic value on element', {
   input: 'local-class={{if true foo}}',
-  elementOutput: 'class="{{lookup-module-styles (unbound styles) {{if true foo}}}}"'
+  elementOutput: 'class="{{lookup-module-styles (unbound styles) (if true foo)}}"'
 });
 
 testTransformationStatement('creating a class attribute with dynamic value on statement', {
@@ -50,7 +50,7 @@ testTransformationStatement('creating a class attribute with dynamic value on st
 
 testTransformationElement('appending a class attribute with dynamic value on element', {
   input: 'class="x" local-class={{if true foo}}',
-  elementOutput: 'class="x {{lookup-module-styles (unbound styles) {{if true foo}}}}"'
+  elementOutput: 'class="x {{lookup-module-styles (unbound styles) (if true foo)}}"'
 });
 
 testTransformationStatement('appending a class attribute with dynamic value on statement', {
@@ -61,40 +61,33 @@ testTransformationStatement('appending a class attribute with dynamic value on s
 function testTransformation(title, options) {
   test('ClassTransformPlugin: ' + title, function(assert) {
     assert.plan(3);
-    assertElementTransforms('<div [ATTRS]></div>', 'ElementNode', options.input, options.elementOutput, assert);
-    assertStatementTransforms('{{x-div [ATTRS]}}', 'MustacheStatement', options.input, options.statementOutput, assert);
-    assertStatementTransforms('{{#x-div [ATTRS]}}content{{/x-div}}', 'BlockStatement', options.input, options.statementOutput, assert);
+    assertTransforms('<div [ATTRS]></div>', 'ElementNode', options.input, options.elementOutput, assert);
+    assertTransforms('{{x-div [ATTRS]}}', 'MustacheStatement', options.input, options.statementOutput, assert);
+    assertTransforms('{{#x-div [ATTRS]}}content{{/x-div}}', 'BlockStatement', options.input, options.statementOutput, assert);
   });
 }
 
 function testTransformationElement(title, options) {
   test('ClassTransformPlugin: ' + title, function(assert) {
     assert.plan(1);
-    assertElementTransforms('<div [ATTRS]></div>', 'ElementNode', options.input, options.elementOutput, assert);
+    assertTransforms('<div [ATTRS]></div>', 'ElementNode', options.input, options.elementOutput, assert);
   });
 }
 
 function testTransformationStatement(title, options) {
   test('ClassTransformPlugin: ' + title, function(assert) {
     assert.plan(2);
-    assertStatementTransforms('{{x-div [ATTRS]}}', 'MustacheStatement', options.input, options.statementOutput, assert);
-    assertStatementTransforms('{{#x-div [ATTRS]}}content{{/x-div}}', 'BlockStatement', options.input, options.statementOutput, assert);
+    assertTransforms('{{x-div [ATTRS]}}', 'MustacheStatement', options.input, options.statementOutput, assert);
+    assertTransforms('{{#x-div [ATTRS]}}content{{/x-div}}', 'BlockStatement', options.input, options.statementOutput, assert);
   });
 }
 
-function assertElementTransforms(template, type, input, output, assert) {
-  var input = template.replace('[ATTRS]', input);
-  var output = template.replace('[ATTRS]', output);
-
-  var actual = htmlbars.print(htmlbars.parse(input, { plugins: { ast: [ClassTransformPlugin] } }));
-  assert.equal(actual, output, 'works for ' + type);
-}
-
-function assertStatementTransforms(template, type, input, output, assert) {
+function assertTransforms(template, type, input, output, assert) {
   var input = template.replace('[ATTRS]', input);
   var output = template.replace('[ATTRS]', output);
 
   var actual = htmlbars.print(htmlbars.parse(input, { plugins: { ast: [ClassTransformPlugin] } }));
   var expected = htmlbars.print(htmlbars.parse(output));
+
   assert.equal(actual, expected, 'works for ' + type);
 }
