dc074e87abf2be5f47114b4cb83551749362b1bb
Dan Freeman <dfreeman@salsify.com>
2016-11-21 18:03:53 -0500

    Additional tests for local-class helper

---
+++ tests/integration/local-class-test.js
@@ -0,0 +1,43 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
+import hbs from 'htmlbars-inline-precompile';
+import getOwner from 'ember-getowner-polyfill';
+
+moduleForComponent('helper:local-class', 'Integration | Helper | local-class', {
+  integration: true
+});
+
+test('it works as a mustache', function(assert) {
+  this.set('__styles__', { foo: '--foo-value' });
+  this.render(hbs`{{local-class 'foo'}}`);
+  assert.equal(this.$().text().trim(), '--foo-value');
+});
+
+test('it works as a positional', function(assert) {
+  const owner = getOwner(this);
+  owner.register('template:components/test-component', hbs`{{yield (local-class 'foo')}}`);
+  owner.register('styles:components/test-component', { foo: '--foo-value' });
+
+  this.render(hbs`
+    {{#test-component as |value|}}
+      {{value}}
+    {{/test-component}}
+  `);
+
+  assert.equal(this.$().text().trim(), '--foo-value');
+});
+
+test('it works as a named parameter', function(assert) {
+  const owner = getOwner(this);
+  owner.register('helper:hash', Ember.Helper.helper((params, hash) => hash));
+  owner.register('template:components/test-component', hbs`{{yield (hash value=(local-class 'foo'))}}`);
+  owner.register('styles:components/test-component', { foo: '--foo-value' });
+
+  this.render(hbs`
+    {{#test-component as |stuff|}}
+      {{stuff.value}}
+    {{/test-component}}
+  `);
+
+  assert.equal(this.$().text().trim(), '--foo-value');
+});
