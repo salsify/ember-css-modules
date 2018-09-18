2c806565979108027f89ee813e0f09f81245c8ef
Dan Freeman <dfreeman@salsify.com>
2018-09-18 17:53:06 -0400

    Improve error message for missing styles module

--- addon/helpers/local-class.js
+++ addon/helpers/local-class.js
@@ -1,5 +1,6 @@
 import { helper } from '@ember/component/helper';
 import { assert } from '@ember/debug';
+import require from 'require';
 
 export function localClass(params, hash) {
   assert('No source specified to local-class lookup', 'from' in hash);
@@ -15,7 +16,11 @@ export default helper(localClass);
 
 function resolveSource(source) {
   if (typeof source === 'string') {
-    return window.require(source).default;
+    if (require.has(source)) {
+      return require(source).default;
+    } else {
+      throw new Error(`Unable to resolve local class names from ${source}; does the styles file exist?`);
+    }
   } else {
     return source;
   }
